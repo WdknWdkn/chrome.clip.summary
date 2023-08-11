139edd3a006f76f36a0a2d40b15610960baa7410
和田健太郎 <49826943+WdknWdkn@users.noreply.github.com>
2023-08-11 11:07:14 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-let selectedText = "";
-let promptName = "";
+var selectedText = "";
+var promptName = "";
 
 function createContextMenus() {
-  // 既存のメニューを削除
-  chrome.contextMenus.removeAll();
+    // 既存のメニューを削除
+    chrome.contextMenus.removeAll();
 
-  // 登録されているプロンプトを取得してメニューを作成
-  chrome.storage.sync.get('prompts', function(data) {
-    const prompts = data.prompts || {};
+    // 登録されているプロンプトを取得してメニューを作成
+    chrome.storage.sync.get('prompts', function(data) {
+    let prompts = data.prompts || {};
     for (const [name,] of Object.entries(prompts)) {
       chrome.contextMenus.create({
         id: name,
@@ -36,7 +36,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 // プロンプトの変更を監視
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-  if (namespace === 'sync' && changes.prompts) {
-    createContextMenus(); // プロンプト変更時にメニューを更新
-  }
+    if (namespace === 'sync' && changes.prompts) {
+        createContextMenus(); // プロンプト変更時にメニューを更新
+    }
 });
